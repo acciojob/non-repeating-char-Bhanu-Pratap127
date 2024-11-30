82e838982003ffc3b5e0039a52d477b11388b5e2
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-11-30 22:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {}
+
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i]
+
+		if(count[char])
+		{
+			count[char]++;
+		}
+
+		else
+		{
+			count[char]=1;
+		}
+	}
+
+	for (let i = 0; i < str.length; i++) {
+		let char = str[i];
+		if (count[char]===1) 
+		{
+			return char;
+		}
+		else 
+		{
+			return null;
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
